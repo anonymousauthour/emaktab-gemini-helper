45212231c45c7fca4bf160d23919846aef9c9f65
anonymousauthour <damirmee2002@gmail.com>
2025-05-09 21:00:46 +0500

    Update emaktab-solver.js

--- emaktab-solver.js
+++ emaktab-solver.js
@@ -8,7 +8,7 @@
 
     // --- НАСТРОЙКИ ---
     const GEMINI_API_KEY = 'AIzaSyB9vWInkcJrlGJmhRteOSthybGnSDUwfGw'; // !!! ОБЯЗАТЕЛЬНО ЗАМЕНИ НА СВОЙ КЛЮЧ !!!
-    const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`; // !!!!!!!!!!!
+    const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-exp-03-25:generateContent?key=${GEMINI_API_KEY}`; // !!!!!!!!!!!
 
     // --- Селекторы ---
     const QUESTION_BLOCK_SELECTOR = '[data-test-id^="block-"]';
